b77ce26a5a458677646bb20c20521a90696e0492
nandini <nandini.soni8@gmail.com>
2016-11-13 10:07:13 +0530

    latest commit

--- kpi_app/views.py
+++ kpi_app/views.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.views import login
 from django.http import HttpResponse
-=======
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout as auth_logout
->>>>>>> d90bf8008d3515d478381253d8b50a866008c5ef
 
 
 # Create your views here.
@@ -17,13 +15,11 @@ def login_page(request):
 
 def home(request):
 	context_dict = {}
-<<<<<<< HEAD
 	return render(request,"kpi_app/home.html", context_dict)
 
 def logout(request):
 	auth_logout(request)
 	return redirect('/')
-=======
 	if request.user.is_authenticated():
 		return render(request,"kpi_app/home.html", context_dict)
 	else:
@@ -33,4 +29,3 @@ def logout(request):
 def logout(request):
     auth_logout(request)
     return redirect('/')
->>>>>>> d90bf8008d3515d478381253d8b50a866008c5ef
