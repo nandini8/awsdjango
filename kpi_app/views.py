4f408d4b07e36d86b8c96a389260cf3edcf76b7a
nandini <nandini.soni8@gmail.com>
2016-11-25 21:57:05 +0530

    mysql database

--- kpi_app/views.py
+++ kpi_app/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.views import login
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout as auth_logout
+from kpi_app.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
 
 
 # Create your views here.
@@ -14,14 +17,14 @@ def login_page(request):
 	return render(request, "kpi_app/login.html", context_dict)
 
 def home(request):
-	context_dict = {}
-	return render(request,"kpi_app/home.html", context_dict)
-
-def logout(request):
-	auth_logout(request)
-	return redirect('/')
 	if request.user.is_authenticated():
-		return render(request,"kpi_app/home.html", context_dict)
+		email = request.user.email
+		try:
+			if User.objects.get(email=email):
+				return render(request,"kpi_app/home.html")
+		except ObjectDoesNotExist:
+			logout(request)
+			return redirect('/')
 	else:
 		return redirect('/')
 
