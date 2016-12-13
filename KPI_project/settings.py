a28df1435d6b66012fbe1b82e4f0ab3b0881d1b0
sahil <chanchadsahil@gmail.com>
2016-12-13 17:54:50 +0530

    URL parse error

--- KPI_project/settings.py
+++ KPI_project/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os, sys
-import urlparse
+import urllib.parse
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'KPI_project.wsgi.application'
     }
 }'''
 # Register database schemes in URLs.
-urlparse.uses_netloc.append('mysql')
+parse.uses_netloc.append('mysql')
 
 try:
 
