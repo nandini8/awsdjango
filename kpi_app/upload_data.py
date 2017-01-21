c7189e733aa68656f66b7fedf20b0e8a84eb4432
nandini <nandini.soni8@gmail.com>
2017-01-21 15:38:39 +0530

    upload script changed to upload dim values

--- kpi_app/upload_data.py
+++ kpi_app/upload_data.py
@@ -8,12 +8,15 @@ def handle_uploaded_file(f, user_obj):
 			destination.write(chunk)
 	with open('templates/kpi_app/a.csv', 'r') as destination:
 		x = csv.DictReader(destination)
-		dim1_obj = Dimension.objects.get_or_create(dim_type= company_obj.filter1_dimValue, company_name = company_obj)[0]
-		dim2_obj = Dimension.objects.get_or_create(dim_type= company_obj.filter2_dimValue, company_name = company_obj)[0]
-		attr1_obj = Attribute.objects.get_or_create(attr_type = company_obj.tab3_name, company_name=company_obj)[0]
+		#dim1_obj = Dimension.objects.get_or_create(dim_type= company_obj.filter1_dimValue, company_name = company_obj)[0]
+		#dim2_obj = Dimension.objects.get_or_create(dim_type= company_obj.filter2_dimValue, company_name = company_obj)[0]
+		#attr1_obj = Attribute.objects.get_or_create(attr_type = company_obj.tab3_name, company_name=company_obj)[0]
 		#dim3_obj = Dimension.objects.get_or_create(dim_type= company_obj.filter3_dimValue, company_name = company_obj)
-		print(type(dim1_obj))
 		for row in x:
-			dimValue1_obj = DimensionValue.objects.get_or_create(dim_type_id = dim1_obj, dim_name = row[company_obj.filter1_dimValue], level=1 )
-			dimValue2_obj = DimensionValue.objects.get_or_create(dim_type_id = dim2_obj, dim_name = row[company_obj.filter2_dimValue], level=1 )
-			attrValue1_obj = AttributeValue.objects.get_or_create(attr_type_id = attr1_obj, attr_name = row[company_obj.tab3_name])
+			dim_obj = Dimension.objects.get_or_create(dim_type= row['Dimension'], company_name = company_obj)[0]
+			print(dim_obj)
+			break
+		for row in x:
+			dimValue1_obj = DimensionValue.objects.get_or_create(dim_type_id = dim_obj, dim_name = row['DimName'], level=row['Level'], parent=DimensionValue.objects.get(id=row['ParentId']))[0]
+			#dimValue2_obj = DimensionValue.objects.get_or_create(dim_type_id = dim2_obj, dim_name = row[company_obj.filter2_dimValue], level=1 )
+			#attrValue1_obj = AttributeValue.objects.get_or_create(attr_type_id = attr1_obj, attr_name = row[company_obj.tab3_name])
