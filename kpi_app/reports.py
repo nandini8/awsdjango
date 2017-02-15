1c611b32aaa0f617cf57b27d93191d515e237806
nandini <nandini.soni8@gmail.com>
2017-02-15 22:44:23 +0530

    filters completed

--- kpi_app/reports.py
+++ kpi_app/reports.py
@@ -5,7 +5,7 @@ from django.db.models import Max, Sum
 from django.db import connection
 
 from kpi_app.models import *
-
+from django.db.models import Q
 
 def filter(request):
 	filter1_option = request.POST['filter1']
@@ -13,28 +13,38 @@ def filter(request):
 	filter3_option = request.POST['filter3']
 	filter4_option = request.POST['filter4']
 	filter5_option = request.POST['filter5']
-	return (filter1_option, filter2_option, filter3_option, filter4_option, filter5_option)
+
+	dimv_obj_list = DimensionValue.objects.filter(Q(dim_name = filter1_option) | Q(dim_name = filter2_option) | Q(dim_name = filter3_option))
+	dimv_obj_dict = {'dim_1': '', 'dim_2':'', 'dim_3':'', 'year': filter4_option, 'month': filter5_option}
+	for obj in dimv_obj_list:
+		if obj.level == 1:
+			dimv_obj_dict['dim_1'] = str(obj.id)
+		elif obj.level == 2:
+			dimv_obj_dict['dim_2'] = str(obj.id)
+		else:
+			dimv_obj_dict['dim_3'] = str(obj.id)
+
+	return(dimv_obj_dict)
+	#return (filter1_option, filter2_option, filter3_option, filter4_option, filter5_option)
 
 def getreports(user_obj, request):
-	filter1_option, filter2_option, filter3_option, filter4_option, filter5_option = filter(request)
+	dimv_obj_dict = filter(request)
+	print(dimv_obj_dict)
 	company_obj = Company.objects.get(id=user_obj.company_name.id)
 	attr_obj = Attribute.objects.filter(company_name_id=company_obj)[0]
 	attrv_obj = AttributeValue.objects.filter(attr_type_id = attr_obj)
 
-	if filter1_option == 'all':
-				MetricData_obj = MetricData.objects.raw('select id, attr_1_id, numerator from kpi_app_metricdata where date_associated = (select max(date_associated) from kpi_app_metricdata) and company_name_id = ' + str(company_obj.id))
-	else:
-		dimv_obj = DimensionValue.objects.get(dim_name = filter1_option)
-		MetricData_obj = MetricData.objects.raw('select id, attr_1_id, numerator from kpi_app_metricdata where month(date_associated) = if("'+ filter5_option +'", "'+filter5_option+'", month(date_associated)) and company_name_id = ' + str(company_obj.id) +' and dim_1_id = "' + str(dimv_obj.id) + '"; ')
+	#if filter1_option == 'all':
+				#MetricData_obj = MetricData.objects.raw('select id, attr_1_id, numerator from kpi_app_metricdata where date_associated = (select max(date_associated) from kpi_app_metricdata) and company_name_id = ' + str(company_obj.id))
+	#else:
+		#dimv_obj = DimensionValue.objects.get(dim_name = filter1_option)
+		#MetricData_obj = MetricData.objects.raw('select id, attr_1_id, numerator from kpi_app_metricdata where month(date_associated) = if("'+ filter5_option +'", "'+filter5_option+'", month(date_associated)) and company_name_id = ' + str(company_obj.id) +' and dim_1_id = "' + str(dimv_obj.id) + '"; ')
 
 
 	#General query not completed yet
-	'''MetricData_obj = MetricData.objects.raw(
-										'select id, attr_1_id, numerator from kpi_app_metricdata where\
-										 month(date_associated) = if("'+ filter5_option +'", "'+filter5_option+'", month(date_associated))\
-										and dim_1_id = if("'+ filter1_option +'", "'+filter1_option+'", dim_1_id)\
-										and dim_2_id = if("'+ filter2_option +'", "'+filter2_option+'", dim_2_id)\
-										and dim_3_id = if("'+ filter3_option +'", "'+filter3_option+'", dim_3_id)')''' 
+	str1 = 'select id, attr_1_id, numerator from kpi_app_metricdata where month(date_associated) = if("'+ dimv_obj_dict['month'] +'", "'+dimv_obj_dict['month']+'", month(date_associated)) and dim_1_id = if("'+ dimv_obj_dict['dim_1'] +'", "'+dimv_obj_dict['dim_1']+'", dim_1_id) or dim_2_id = if("'+ dimv_obj_dict['dim_2'] +'", "'+dimv_obj_dict['dim_2']+'", dim_2_id) or dim_3_id = if("'+ dimv_obj_dict['dim_3'] +'", "'+dimv_obj_dict['dim_3']+'", dim_3_id)'
+	#print(str1)
+	MetricData_obj = MetricData.objects.raw(str1)
 
 	report_data = list()
 	for x in attrv_obj:
@@ -50,12 +60,13 @@ def getreports(user_obj, request):
 			temp_av = AttributeValue.objects.get(attr_name = y.attr_1)
 			if temp_av.attr_name == name:
 				scores['Name'] = name
-				attendance = get_attendance(temp_av)
+				attendance = get_attendance(temp_av, dimv_obj_dict['month'])
 				temp_dv = DimensionValue.objects.get(id=y.dim_1_id)
-				if temp_dv.dim_name == 'Attendance': # and int(y.numerator) > 0 :
-					scores[temp_dv.dim_name]= int(attendance)
-				else:
-					scores[temp_dv.dim_name]= int(y.numerator)
+				#if temp_dv.dim_name == 'Attendance': # and int(y.numerator) > 0 :
+					#scores[temp_dv.dim_name]= int(attendance)
+				#else:
+				scores['Attendance'] = int(attendance)
+				scores[temp_dv.dim_name]= int(y.numerator)
 					#m_obj = MetricData.objects.filter(attr_1_id = temp_av.id, dim_1_id = temp_dv.id).aggregate(Max('numerator'))
 					#scores[temp_dv.dim_name] = int(m_obj['numerator__max'])
 		report_data.append(scores)
@@ -68,9 +79,9 @@ def getreports(user_obj, request):
 					]
 	return(report_data, headers)
 
-def get_attendance(temp_av):
+def get_attendance(temp_av, month):
 	c = connection.cursor()
-	c.execute('select sum(numerator) from kpi_app_metricdata where dim_1_id in (select id from kpi_app_dimensionvalue where dim_name = "Attendance" ) and attr_1_id = ' + str(temp_av.id))
+	c.execute('select sum(numerator) from kpi_app_metricdata where dim_1_id in (select id from kpi_app_dimensionvalue where dim_name = "Attendance" ) and attr_1_id = ' + str(temp_av.id) + ' and month(date_associated) = if("'+ month +'", "'+ month +'", month(date_associated))')
 	rows = c.fetchone()[0]
 	return rows
 
