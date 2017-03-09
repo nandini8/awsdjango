341bd972a1cf680c98d8cf36e259e9aa9e419b6e
nandini <nandini.soni8@gmail.com>
2017-03-09 14:18:33 +0530

    THIS ONLY WORKING

--- KPI_project/apache/wsgi.py
+++ KPI_project/apache/wsgi.py
@@ -7,7 +7,7 @@ sys.path.append(workspace)
 sys.path.append(project)
 
 # Add the path to 3rd party django application and to django itself.
-sys.path.append('/home/django_project/awsdjango/serene-reef-39478')
+sys.path.append('/home/django_project/KPIaws/awsdjango')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'KPI_project.apache.override'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
