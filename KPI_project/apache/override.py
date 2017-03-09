d968defb1a5fda34d3b36bbf5c17da116906cffd
nandini <nandini.soni8@gmail.com>
2017-03-09 15:50:28 +0530

    Google api configured

--- KPI_project/apache/override.py
+++ KPI_project/apache/override.py
@@ -1,4 +1,4 @@
 from KPI_project.settings import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000','http://kpienv.rnpcmbr4yq.ap-south-1.elasticbeanstalk.com/','https://kpienv.rnpcmbr4yq.ap-south-1.elasticbeanstalk.com/','127.0.0.1:80']
