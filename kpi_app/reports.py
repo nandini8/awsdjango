edc17658bac1ab1b93bcc21f6304baf3a875ad1a
nandini <nandini.soni8@gmail.com>
2017-03-14 19:53:39 +0530

    before apply reports generated

--- kpi_app/reports.py
+++ kpi_app/reports.py
@@ -7,6 +7,7 @@ from kpi_app.models import *
 from django.db.models import Q
 
 def filter(request):
+
 	filter1_option = request.POST['filter1']
 	filter2_option = request.POST['filter2']
 	filter3_option = request.POST['filter3']
@@ -27,10 +28,10 @@ def filter(request):
 	#return (filter1_option, filter2_option, filter3_option, filter4_option, filter5_option)
 
 def getreports(user_obj, request):
-	print()
 	dimv_obj_dict = filter(request)
-	print(dimv_obj_dict)
+	#print(dimv_obj_dict)
 	company_obj = Company.objects.get(id=user_obj.company_name.id)
+	#print(company_obj)
 	attr_obj = Attribute.objects.filter(company_name_id=company_obj)[0]
 	attrv_obj = AttributeValue.objects.filter(attr_type_id = attr_obj)
 
@@ -43,7 +44,7 @@ def getreports(user_obj, request):
 
 	#General query not completed yet
 	str1 = 'select id, attr_1_id, numerator from kpi_app_metricdata where month(date_associated) = if("'+ dimv_obj_dict['month'] +'", "'+dimv_obj_dict['month']+'", month(date_associated)) and dim_1_id = if("'+ dimv_obj_dict['dim_1'] +'", "'+dimv_obj_dict['dim_1']+'", dim_1_id) or dim_2_id = if("'+ dimv_obj_dict['dim_2'] +'", "'+dimv_obj_dict['dim_2']+'", dim_2_id) or dim_3_id = if("'+ dimv_obj_dict['dim_3'] +'", "'+dimv_obj_dict['dim_3']+'", dim_3_id)'
-	print(str1)
+	#print(str1)
 	MetricData_obj = MetricData.objects.raw(str1)
 
 	report_data = list()
@@ -64,6 +65,52 @@ def getreports(user_obj, request):
 				temp_dv = DimensionValue.objects.get(id=y.dim_1_id)
 				if temp_dv.dim_name == 'Attendance': # and int(y.numerator) > 0 :
 					attendance = get_attendance(temp_av, dimv_obj_dict['month'])
+					scores[temp_dv.dim_name]= int(attendance)
+				else:
+					#scores['Attendance'] = int(attendance)
+					#print(scores['Name'],scores['Attendance'])
+					scores[temp_dv.dim_name]= int(y.numerator)
+					#m_obj = MetricData.objects.filter(attr_1_id = temp_av.id, dim_1_id = temp_dv.id).aggregate(Max('numerator'))
+					#scores[temp_dv.dim_name] = int(m_obj['numerator__max'])
+		report_data.append(scores)
+	#print(report_data)
+		
+	headers = [ 'Name', 'Attendance','Hackerrank Algorithm Score',
+					'Hackerrank Python Score',
+					'Hackerrank Data Structure Score',
+					'Project Euler - Number of problems solved',
+					'Rosalind Info - Number of problems solved'
+					]
+	return report_data, headers
+
+
+def getreportsBeforeApply(user_obj,request):
+	company_obj = Company.objects.get(id=user_obj.company_name.id)
+	#print(company_obj)
+	attr_obj = Attribute.objects.filter(company_name_id=company_obj)[0]
+	attrv_obj = AttributeValue.objects.filter(attr_type_id = attr_obj)
+
+	str1 = "select id, attr_1_id, numerator from kpi_app_metricdata where company_name_id = " + str(company_obj.id)
+	#print(str1)
+	MetricData_obj = MetricData.objects.raw(str1)
+	report_data = list()
+	for x in attrv_obj:
+		scores = dict()
+		scores.update({ 'Name': "", 'Attendance': 0,'Hackerrank Algorithm Score':0,
+					'Hackerrank Python Score':0,
+					'Hackerrank Data Structure Score':0,
+					'Project Euler - Number of problems solved':0,
+					'Rosalind Info - Number of problems solved':0
+					})
+		name = x.attr_name
+		for y in MetricData_obj:
+			temp_av = AttributeValue.objects.get(attr_name = y.attr_1)
+			if temp_av.attr_name == name:
+				scores['Name'] = name
+				#print(attendance)
+				temp_dv = DimensionValue.objects.get(id=y.dim_1_id)
+				if temp_dv.dim_name == 'Attendance': # and int(y.numerator) > 0 :
+					attendance = get_attendance(temp_av, "")
 					scores[temp_dv.dim_name]= int(attendance)
 				else:
 					#scores['Attendance'] = int(attendance)
@@ -107,6 +154,3 @@ def get_avg(user_obj,request):
 		#print(round(int(rows)),x.dim_name)
 		l.update({x.dim_name:round(int(rows))})
 	return(l)
-
-
-
