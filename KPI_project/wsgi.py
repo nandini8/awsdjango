bf5fba359dcf51729c195700cdb434be81b21ae4
nandini <nandini.soni8@gmail.com>
2017-06-21 14:49:08 +0530

    local

--- KPI_project/wsgi.py
+++ KPI_project/wsgi.py
@@ -9,8 +9,25 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+'''from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KPI_project.settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application()'''
+
+import time
+import traceback
+import signal
+import sys
+from django.core.wsgi import get_wsgi_application
+
+try:
+    application = get_wsgi_application()
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
