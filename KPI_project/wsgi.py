9fd10ca2e9c13c488760b0aae9eefd4972cffb7d
nandini <nandini.soni8@gmail.com>
2017-06-21 16:39:16 +0530

    lets see

--- KPI_project/wsgi.py
+++ KPI_project/wsgi.py
@@ -14,10 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KPI_project.settings")
 
 application = get_wsgi_application()
-
-
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '': 
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
